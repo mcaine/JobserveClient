351526ccd3ccea69a92e6cd7ddff0e84f37667d7
mcaine <mikey.caine@gmail.com>
2015-06-04 23:31:44 +0100

    Tests

--- src/main/java/com/mikeycaine/jobserve/Application.java
+++ src/main/java/com/mikeycaine/jobserve/Application.java
@@ -26,90 +26,53 @@ public class Application implements CommandLineRunner {
 		//jobSearchDefaults();
 		//jobSearchValueLists();
 		//jobTypes();
-		jobSearchPOST();
+		//jobSearchPOST();
 		//jobSearchGET();
 		//singleJobDetails();
+		//geolocationMatch();
 	}
 
-	void countries() throws InterruptedException, ExecutionException {
-		// Start the clock
-		long start = System.currentTimeMillis();
 
-		System.out.println("[TIME 1] " + (System.currentTimeMillis() - start));
-		Future<CountryCollection> countries = jobserve.getAllCountries();
-		System.out.println("[TIME 2] " + (System.currentTimeMillis() - start));
-
-		System.out.println("[TIME 3] " + (System.currentTimeMillis() - start));
-
-		List<Country> countryList = countries.get().getCountry();
-
-		System.out.println("[TIME 4] " + (System.currentTimeMillis() - start));
-
-		for (Country country : countryList) {
-			System.out.println(country.displayName.getValue());
-		}
-	}
-
-	void industries() throws InterruptedException, ExecutionException {
-		// Start the clock
-		long start = System.currentTimeMillis();
-
-		System.out.println("[TIME 1] " + (System.currentTimeMillis() - start));
-		Future<IndustryCollection> industries = jobserve.getAllIndustries();
-		System.out.println("[TIME 2] " + (System.currentTimeMillis() - start));
-
-
-
-		System.out.println("[TIME 3] " + (System.currentTimeMillis() - start));
-
-		List<Industry> industryList = industries.get().getIndustry();
-
-		System.out.println("[TIME 4] " + (System.currentTimeMillis() - start));
-
-		for (Industry industry : industryList) {
-			System.out.println(industry.displayName.getValue());
-		}
-	}
 	
-	void salaryFrequencies() throws InterruptedException, ExecutionException {
-		// Start the clock
-		long start = System.currentTimeMillis();
-
-		System.out.println("[TIME 1] " + (System.currentTimeMillis() - start));
-		Future<SalaryFrequencyCollection> salaryFrequencies = jobserve.getAllSalaryFrequencies();
-		System.out.println("[TIME 2] " + (System.currentTimeMillis() - start));
-
-		System.out.println("[TIME 3] " + (System.currentTimeMillis() - start));
-
-		List<SalaryFrequency> salaryFreqList = salaryFrequencies.get().getSalaryFrequency();
-
-		System.out.println("[TIME 4] " + (System.currentTimeMillis() - start));
-
-		for (SalaryFrequency freq : salaryFreqList) {
-			System.out.println(freq.getText().getValue());
-		}
-	}
+//	void salaryFrequencies() throws InterruptedException, ExecutionException {
+//		// Start the clock
+//		long start = System.currentTimeMillis();
+//
+//		System.out.println("[TIME 1] " + (System.currentTimeMillis() - start));
+//		Future<SalaryFrequencyCollection> salaryFrequencies = jobserve.getAllSalaryFrequencies();
+//		System.out.println("[TIME 2] " + (System.currentTimeMillis() - start));
+//
+//		System.out.println("[TIME 3] " + (System.currentTimeMillis() - start));
+//
+//		List<SalaryFrequency> salaryFreqList = salaryFrequencies.get().getSalaryFrequency();
+//
+//		System.out.println("[TIME 4] " + (System.currentTimeMillis() - start));
+//
+//		for (SalaryFrequency freq : salaryFreqList) {
+//			System.out.println(freq.getText().getValue());
+//		}
+//	}
 	
-	void currencies() throws InterruptedException, ExecutionException {
-		Future<CurrencyCollection> currencies = jobserve.getAllSupportedCurrencies();
-		List<Currency> currencyList = currencies.get().getCurrency();
-		for (Currency curr : currencyList) {
-			System.out.println(curr.getName().getValue());
-		}
-	}
+//	void currencies() throws InterruptedException, ExecutionException {
+//		Future<CurrencyCollection> currencies = jobserve.getAllSupportedCurrencies();
+//		List<Currency> currencyList = currencies.get().getCurrency();
+//		for (Currency curr : currencyList) {
+//			System.out.println(curr.getName().getValue());
+//		}
+//	}
 	
 	void jobSearchDefaults() throws InterruptedException, ExecutionException {
 		Future<JobSearch> jobSearch = jobserve.getJobSearchDefaults();
 		System.out.println("Job Search Defaults Max Distance: " + jobSearch.get().getMaxDistance().getValue());
 	}
 	
-	void jobSearchValueLists() throws InterruptedException, ExecutionException {
-		Future<ValueListsIndex> lists = jobserve.getJobSearchValueLists();
-		List<ValueListsIndex.ValueList> valueLists = lists.get().getValueList();
-		for (ValueListsIndex.ValueList valueList : valueLists) {
-			System.out.println("Value list: " + valueList.getName());
-		}
-	}
+//	void jobSearchValueLists() throws InterruptedException, ExecutionException {
+//		Future<ValueListsIndex> lists = jobserve.getJobSearchValueLists();
+//		List<ValueListsIndex.ValueList> valueLists = lists.get().getValueList();
+//		for (ValueListsIndex.ValueList valueList : valueLists) {
+//			System.out.println("Value list: " + valueList.getName());
+//		}
+//	}
 	
 	void jobTypes() throws InterruptedException, ExecutionException {
 		Future<JobTypeCollection> lists = jobserve.getJobTypes();
@@ -139,6 +102,22 @@ public class Application implements CommandLineRunner {
 		JobSearchResults jobSearchResults = results.get();
 		System.out.println("Found " + jobSearchResults.getJobCount() + " jobs");
 		
+		
+		System.out.println("Page " + jobSearchResults.getPageNo() + " of " + jobSearchResults.getPageCount() + " pages");
+		System.out.println("Page size is " + jobSearchResults.getPageSize());
+		
+		IDCollection jobIDCollection = jobSearchResults.getJobIDs().getValue();
+		for (String id : jobIDCollection.getID()) {
+			System.out.println("Job ID " + id);
+		}
+		
+		JobCollection jobCollection = jobSearchResults.getJobs().getValue();
+		for (Job job : jobCollection.getJob()) {
+			System.out.println();
+			System.out.println(job.getShortDescription().getValue());
+			System.out.println(job.getRecruiterName().getValue());
+		}
+		
 	}
 	
 	void jobSearchGET() throws InterruptedException, ExecutionException {
@@ -158,7 +137,22 @@ public class Application implements CommandLineRunner {
 		JobSearchResults jobSearchResults = results.get();
 		System.out.println("Found " + jobSearchResults.getJobCount() + " jobs");
 		
+		System.out.println("Page " + jobSearchResults.getPageNo() + " of " + jobSearchResults.getPageCount() + " pages");
+		System.out.println("Page size is " + jobSearchResults.getPageSize());
+		
 	}
+	
+//	void geolocationMatch() throws InterruptedException, ExecutionException {
+//		double latitude = 51.50442;
+//		double longitude = -0.01835;
+//		double maxDistance = 10000.0;
+//		Future<GeoLocationMatch> geoFuture = jobserve.getLocationMatch(latitude, longitude, maxDistance);
+//		GeoLocationMatch geo = geoFuture.get();
+//		
+//		Location loc = geo.getLocation().getValue();
+//		System.out.println("GEO Location Match " + loc.getText().getValue());
+//	}
+		
 	
 	void singleJobDetails() throws InterruptedException, ExecutionException {
 
